fc481fa888ae09cc62aeedd38ea754d498763324
Mark Hogg <m.hogg@student.unsw.edu.au>
2018-09-03 08:36:03 +1000

    Cleaned up single donor ESR method a bit and added more commenting

--- donors.py
+++ donors.py
@@ -1,4 +1,4 @@
-# Some classes containing useful parameters regarding 31P donors in silicon
+# Classes intended for simulating 31P donors in silicon
 
 import scipy.constants as const
 import matplotlib.pyplot as plt
@@ -12,45 +12,56 @@ class SingleDonor():
 	This class creates a single 31P donor object
 	'''
 	
-	# Define some constants
+	# Define some physical constants
 	_mu_b = const.physical_constants['Bohr magneton'][0]
 	_mu_n = const.physical_constants['nuclear magneton'][0]
-	# Set up the basis I'm going to use. I believe I have defined this correctly...
+	
+	# Set up the basis I'm going to use. In this basis spin up is the state [1,0], spin down [0,1]
+	# The first element is the electron spin, the second element the nuclear spin.
 	upup = qu.tensor(qu.basis(2,0), qu.basis(2,0))
 	updn = qu.tensor(qu.basis(2,0), qu.basis(2,1))
 	dnup = qu.tensor(qu.basis(2,1), qu.basis(2,0))
 	dndn = qu.tensor(qu.basis(2,1), qu.basis(2,1))
 	
 	# Defining electron and nuclear spin operators
-	Sz_operator = qu.tensor(qu.sigmaz(), qu.identity(2))
-	Sy_operator = qu.tensor(qu.sigmay(), qu.identity(2))
 	Sx_operator = qu.tensor(qu.sigmax(), qu.identity(2))
-	Iz_operator = qu.tensor(qu.identity(2), qu.sigmaz())
-	Iy_operator = qu.tensor(qu.identity(2), qu.sigmay())
+	Sy_operator = qu.tensor(qu.sigmay(), qu.identity(2))
+	Sz_operator = qu.tensor(qu.sigmaz(), qu.identity(2))
 	Ix_operator = qu.tensor(qu.identity(2), qu.sigmax())
+	Iy_operator = qu.tensor(qu.identity(2), qu.sigmay())
+	Iz_operator = qu.tensor(qu.identity(2), qu.sigmaz())
+
 		
 	def __init__(self, **kwargs):
 		'''
-		Docstring
+		Initialise the single donor object. Electron and nuclear g factors can be specified, otherwise default values 
+		are taken.
+		Inputs:
+			electron_g_factor (optional) - electron g-factor, value is 2 if not specifiend
+			nuclear_g_factor (optional) - nuclear g-factor
+			hyperfine_coupling (optional) - hyperfine coupling constant between electron and nuclear spin. Default 
+											value is bulk single donor
+			mag_field (optional) - static magnetic field in z direction. Default is 1 Tesla if not specified.
 		'''
-		g_factor = kwargs.get('electron_g_factor', 2)
-		ng_factor = kwargs.get('nuclear_g_factor', 2.2632)
-		self.hyperfine = kwargs.get('default_hyperfine', 2*np.pi*114e6)
-		self.b_z = kwargs.get('default_mag_field', 1)
+		self._g_factor = kwargs.get('electron_g_factor', 2.0)
+		self._ng_factor = kwargs.get('nuclear_g_factor', 2.2632)
+		self.hyperfine = kwargs.get('hyperfine_coupling', 2*np.pi*114e6)
+		self.b_z = kwargs.get('mag_field', 1.0)
 		
-		self.electron_gyro = g_factor*self._mu_b/const.hbar
-		self.nuclear_gyro = ng_factor*self._mu_n/const.hbar
+		self.electron_gyro = self._g_factor*self._mu_b/const.hbar
+		self.nuclear_gyro = self._ng_factor*self._mu_n/const.hbar
 		self.Hamiltonian = self.build_hamiltonian()
 		
 	def build_hamiltonian(self, **kwargs):
 		'''
-		Set up the Hamiltonian for the single donor
+		Construct static single donor Hamiltonian for given physical parameters
+		Inputs:
+			static_mag (optional) - static bz field, defaults to object attribute if not specified
 		'''
-		b_z = kwargs.get('static_mag', self.b_z)
-		hyperfine = kwargs.get('hyperfine', self.hyperfine)
-		H_electron_zeeman = 0.5*self.electron_gyro*b_z*qu.tensor(qu.sigmaz(), qu.identity(2))
-		H_nuclear_zeeman = 0.5*self.nuclear_gyro*b_z*qu.tensor(qu.identity(2), qu.sigmaz())
-		H_hyperfine = hyperfine*(qu.tensor(qu.sigmax(), qu.sigmax()) + qu.tensor(qu.sigmay(), qu.sigmay()) 
+		self.b_z = kwargs.get('static_mag', self.b_z)
+		H_electron_zeeman = 0.5*self.electron_gyro*self.b_z*self.Sz_operator
+		H_nuclear_zeeman = 0.5*self.nuclear_gyro*self.b_z*self.Iz_operator
+		H_hyperfine = self.hyperfine*(qu.tensor(qu.sigmax(), qu.sigmax()) + qu.tensor(qu.sigmay(), qu.sigmay()) 
 				+ qu.tensor(qu.sigmaz(), qu.sigmaz()))
 		H = H_electron_zeeman - H_nuclear_zeeman + H_hyperfine
 		return H
@@ -64,6 +75,8 @@ class SingleDonor():
 			num_points (optional) - number of points to evaluate at (default is 1000)
 		Outputs: 
 			None
+		Todo:
+			- Add plots of projection on to high-field eigenstates?
 		'''
 		num_points = kwargs.get('num_points', 1000)
 		b_sweep = np.linspace(b_start, b_stop, num_points)
@@ -74,19 +87,19 @@ class SingleDonor():
 			self.eigenvals[ind], temp_vecs = self.Hamiltonian.eigenstates()
 			projvecs[ind] = self.project_eigenvecs(temp_vecs)
 		plt.figure(1)
-		plt.plot(b_sweep, self.eigenvals/(2*np.pi))
+		plt.plot(b_sweep, self.eigenvals/(2*np.pi*1e9))
 		plt.xlabel('Magnetic field (T)')
-		plt.ylabel('Transition frequency (Hz)')
-		plt.figure(2)
-		plt.plot(b_sweep, projvecs[:,0:2], '+')
-		plt.xlabel('Magnetic field (T)')
-		plt.ylabel('Projection')
-		plt.legend(['E down, N up', 'E down, N down'])
-		plt.figure(3)
-		plt.plot(b_sweep, projvecs[:,2:4], '+')
-		plt.xlabel('Magnetic field (T)')
-		plt.ylabel('Projection')
-		plt.legend(['E up, N down', 'E up, N up'])
+		plt.ylabel('Energy (GHz)')
+		# plt.figure(2)
+		# plt.plot(b_sweep, projvecs[:,0:2], '.')
+		# plt.xlabel('Magnetic field (T)')
+		# plt.ylabel('Projection')
+		# plt.legend(['E down, N up', 'E down, N down'])
+		# plt.figure(3)
+		# plt.plot(b_sweep, projvecs[:,2:4], '.')
+		# plt.xlabel('Magnetic field (T)')
+		# plt.ylabel('Projection')
+		# plt.legend(['E up, N down', 'E up, N up'])
 		plt.show(block=False)
 		
 	def project_eigenvecs(self, vecs):
@@ -100,28 +113,50 @@ class SingleDonor():
 		vecs[2].overlap(self.updn), vecs[3].overlap(self.upup)])
 		return proj_vector
 	
-	# def do_esr(self, string='lost self', tlist=np.linspace(0,100e-12,1000)):
-		# print(string)
 	
-	def do_spin_resonance(self, b1=0.01, bz=1, freq=28e9, psi0=dnup, tlist=np.linspace(0,1e-9,1000)):
+	def do_spin_resonance(self, b1=0.01, bz=1.0, psi0=dnup, **kwargs):
 		'''
-		Perform an ESR/NMR control operation on the system. I hope to have this set up so that you do ESR
-		by putting in a signal at the ESR frequency, and NMR by putting a signal in at NMR frequency
+		Perform an ESR/NMR control operation on the system. The simulation works with the full Hamiltonian of both
+		the electron and the nucleus, so to choose whether to do an ESR or NMR experiment you just change the drive
+		frequency to match the appropriate resonance.
+		Inputs:
+			b1 - amplitude of ac magnetic drive (default 0.01T)
+			bz - static mag field (default 1.0T)
+			psi0 - initial state (default dnup state)
+			kwargs:
+			freq - Rabi drive frequency (default is energy splitting between dnup and upup states)
+			tmax - Simulation time (default is approximately a pi/2 pulse for ESR drive)
+		Outputs:
+			output - Expectation values of electron and nuclear spin operators (ordered as Sx, Sy, Sz, Ix, Iy, Iz)
+		Todo:
+			- Have detuning from resonance as an input parameters
+			- Come up with a more flexible way of choosing initial states and resonances to drive
 		'''
+		self.Hamiltonian = self.build_hamiltonian(static_mag=bz) 	# Update the static Hamiltonian
+		esr1_freq, esr2_freq, nmr1_freq, nmr2_freq = self.calculate_resonance_freqs()
+		freq = kwargs.get('freq', esr1_freq) 		# AC drive frequency, defaults to energy splitting between dnup and upup
+		tmax = kwargs.get('tmax', np.pi/(self.electron_gyro*b1)) 	# Default should be approximately a pi/2 pulse
+		n_steps = kwargs.get('n_steps', 1000)
+		tlist = np.linspace(0,tmax,n_steps)
 		e_ops_list = [self.Sx_operator, self.Sy_operator, self.Sz_operator, self.Ix_operator, 
 						self.Iy_operator, self.Iz_operator]
 		drive_args = {'b1':b1, 'freq':freq, 'phase':0}
-		self.Hamiltonian = self.build_hamiltonian(static_mag=bz)
-		esr_Hamiltonian = 0.5*self.electron_gyro*b1*self.Sx_operator
-		nmr_Hamiltonian = 0.5*self.nuclear_gyro*b1*self.Ix_operator
+		esr_Hamiltonian = 0.5*self.electron_gyro*self.Sx_operator
+		nmr_Hamiltonian = 0.5*self.nuclear_gyro*self.Ix_operator
 		full_H = [self.Hamiltonian, [esr_Hamiltonian+nmr_Hamiltonian, rabi_drive]]
-		output = qu.mesolve(full_H, psi0, tlist, c_ops=[], e_ops=e_ops_list, args=drive_args)
-		self.plot_trajectories(output.times, np.array(output.expect[0:3]))
-		self.plot_trajectories(output.times, np.array(output.expect[3:6]))
+		opts = qu.Options(nsteps=5000)
+		output = qu.mesolve(full_H, psi0, tlist, c_ops=[], e_ops=e_ops_list, args=drive_args, options=opts)
+		self.plot_trajectories(output.times, np.array(output.expect[0:3]), 'Electron state')
+		self.plot_trajectories(output.times, np.array(output.expect[3:6]), 'Nucleus state')
 		return output
 		
-	def plot_trajectories(self, times, trajs):
+	def plot_trajectories(self, times, trajs, plot_title=''):
+		'''
+		Plot output of a simulation result on the Bloch sphere
+		'''
 		f = plt.figure(figsize=(5,5), facecolor='white')
+		f.suptitle(plot_title, x=0.2)
+		# plt.title(plot_title)
 		xp2 = qu.rx(np.pi*0.5)
 		yp2 = qu.ry(np.pi*0.5)
 		zp2 = qu.rz(np.pi*0.5)
@@ -155,6 +190,7 @@ class SingleDonor():
 		b.sphere_color = '000000'
 		b.render(f)
 		b.show()
+		
 
 	def calculate_resonance_freqs(self):
 		'''
@@ -182,7 +218,6 @@ def detuning_pulse(t, args):
 	'''
 	return (args['stop']-args['start'])*t/args['tmax']
 	
-		
 		
 class DonorSingletTriplet(SingleDonor):
 	'''
@@ -334,36 +369,11 @@ class DonorSingletTriplet(SingleDonor):
 		return output, tlist
 		
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+class CoupledDonors():
+	'''
+	Class for a coupled donor system
+	'''
+	
+	def __init__(self, Donor1, Donor2):
+		'''
+		'''
