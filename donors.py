2908ab8da931842282f6f5d72b2a0f454be80d7d
Mark Hogg <m.hogg@student.unsw.edu.au>
2018-10-23 19:41:30 +1100

    Commiting updates from desk computer, including comments to make things more fit for human consumption

--- donors.py
+++ donors.py
@@ -1,4 +1,5 @@
-# Classes intended for simulating 31P donors in silicon
+# Classes intended for simulating 31P donors in silicon.
+# Initiated by Mark Hogg, 2018
 
 import scipy.constants as const
 import matplotlib.pyplot as plt
@@ -10,7 +11,7 @@ import seaborn as sb
 
 class SingleDonor():
 	'''
-	This class creates a single 31P donor object
+	This is a class for a single 31P donor object.
 	'''
 	
 	# Define some physical constants
@@ -38,34 +39,38 @@ class SingleDonor():
 		Initialise the single donor object. Electron and nuclear g factors can be specified, otherwise default values 
 		are taken.
 		Inputs:
+			mag_field (optional) - static magnetic field in z direction. Default is 1 Tesla if not specified.
 			electron_g_factor (optional) - electron g-factor, value is 2 if not specifiend
 			nuclear_g_factor (optional) - nuclear g-factor
 			hyperfine_coupling (optional) - hyperfine coupling constant between electron and nuclear spin. Default 
 											value is bulk single donor
-			mag_field (optional) - static magnetic field in z direction. Default is 1 Tesla if not specified.
 		'''
+		self.b_z = kwargs.get('mag_field', 1.0)
 		self._g_factor = kwargs.get('electron_g_factor', 2.0)
 		self._ng_factor = kwargs.get('nuclear_g_factor', 2.2632)
 		self.hyperfine = kwargs.get('hyperfine_coupling', 2*np.pi*114e6)
-		self.b_z = kwargs.get('mag_field', 1.0)
 		
 		self.electron_gyro = self._g_factor*self._mu_b/const.hbar
 		self.nuclear_gyro = self._ng_factor*self._mu_n/const.hbar
-		self.Hamiltonian = self.build_hamiltonian()
+		self.build_hamiltonian()
 		
 	def build_hamiltonian(self, **kwargs):
 		'''
 		Construct static single donor Hamiltonian for given physical parameters
 		Inputs:
-			static_mag (optional) - static bz field, defaults to object attribute if not specified
+			mag_field (optional) - static bz field, defaults to object attribute if not specified. Note that this 
+									function updates the object static magnetic field attribute if you do input one,
+									so the object Hamiltonian changes.
+		Todo:
+			- Come up with a tidier way of managing static b_z attribute here.
 		'''
-		self.b_z = kwargs.get('static_mag', self.b_z)
+		self.b_z = kwargs.get('mag_field', self.b_z)
 		H_electron_zeeman = 0.5*self.electron_gyro*self.b_z*self.Sz_operator
 		H_nuclear_zeeman = 0.5*self.nuclear_gyro*self.b_z*self.Iz_operator
 		H_hyperfine = self.hyperfine*(qu.tensor(qu.sigmax(), qu.sigmax()) + qu.tensor(qu.sigmay(), qu.sigmay()) 
 				+ qu.tensor(qu.sigmaz(), qu.sigmaz()))
 		H = H_electron_zeeman - H_nuclear_zeeman + H_hyperfine
-		return H
+		self.Hamiltonian = H
 		
 	def plot_spectrum(self, b_start, b_stop, **kwargs):
 		'''
@@ -81,10 +86,11 @@ class SingleDonor():
 		'''
 		num_points = kwargs.get('num_points', 1000)
 		b_sweep = np.linspace(b_start, b_stop, num_points)
+		# Pre-allocate numpy array for eigenvalues
 		self.eigenvals = np.zeros([len(b_sweep), 4])
 		projvecs = np.zeros([len(b_sweep), 4])
 		for ind,val in enumerate(b_sweep):
-			self.Hamiltonian = self.build_hamiltonian(static_mag=val)
+			self.build_hamiltonian(static_mag=val)
 			self.eigenvals[ind], temp_vecs = self.Hamiltonian.eigenstates()
 			projvecs[ind] = self.project_eigenvecs(temp_vecs)
 		plt.figure(1)
@@ -115,37 +121,58 @@ class SingleDonor():
 		return proj_vector
 	
 	
-	def do_spin_resonance(self, b1=0.01, bz=1.0, psi0=dnup, **kwargs):
+	def do_spin_resonance(self, b1=0.01, bz=1.0, psi0=dnup, transition='esr1', detuning=0, **kwargs):
 		'''
 		Perform an ESR/NMR control operation on the system. The simulation works with the full Hamiltonian of both
 		the electron and the nucleus, so to choose whether to do an ESR or NMR experiment you just change the drive
-		frequency to match the appropriate resonance.
+		frequency to match the appropriate resonance. Currently only sine wave drive supported.
 		Inputs:
 			b1 - amplitude of ac magnetic drive (default 0.01T)
 			bz - static mag field (default 1.0T)
 			psi0 - initial state (default dnup state)
+			transition - select the base transition frequency that you want to drive at. Default is dnup to upup freq
+							options are esr1, esr2, nmr1, nmr2
+			detuning - detuning from resonance frequency of transition you selected (in Hz). Default is 0
 			kwargs:
-			freq - Rabi drive frequency (default is energy splitting between dnup and upup states)
-			tmax - Simulation time (default is approximately a pi/2 pulse for ESR drive)
+			tmax (optional) - Simulation time (default is approximately a pi/2 pulse)
+			n_steps (optional) - number of steps in simulation time vector. Default is 1000
 		Outputs:
 			output - Expectation values of electron and nuclear spin operators (ordered as Sx, Sy, Sz, Ix, Iy, Iz)
 		Todo:
-			- Have detuning from resonance as an input parameters
-			- Come up with a more flexible way of choosing initial states and resonances to drive
 			- Have solver options as kwargs input
+			- Plot expectation values as a function of time
+			- Include decoherence channels
 		'''
-		self.Hamiltonian = self.build_hamiltonian(static_mag=bz) 	# Update the static Hamiltonian
-		esr1_freq, esr2_freq, nmr1_freq, nmr2_freq = self.calculate_resonance_freqs()
-		freq = kwargs.get('freq', esr1_freq) 		# AC drive frequency, defaults to energy splitting between dnup and upup
-		tmax = kwargs.get('tmax', np.pi/(self.electron_gyro*b1)) 	# Default should be approximately a pi/2 pulse
+		self.build_hamiltonian(static_mag=bz) 	# Update the static Hamiltonian
+		esr1_freq, esr2_freq, nmr1_freq, nmr2_freq = self.calculate_resonance_freqs() # Get the transition frequencies
+		# Set ESR drive frequency based on input options
+		if transition == 'esr1':
+			freq = esr1_freq + detuning		# AC drive frequency, defaults to energy splitting between dnup and upup
+			esr_flag = True
+		elif transition == 'esr2':
+			freq = esr2_freq + detuning
+			esr_flag = True
+		elif transition == 'nmr1':
+			freq = nmr1_freq + detuning
+			esr_flag = False
+		elif transition == 'nmr2':
+			freq = nmr2_freq + detuning
+			esr_flag = False
+		else:
+			raise ValueError('Undefined spin resonance transition - should be esr1, esr2, nmr1 or nmr2')
+		
+		if esr_flag:
+			tmax = kwargs.get('tmax', np.pi/(self.electron_gyro*b1)) 	# Default should be approximately a pi/2 pulse
+		else:
+			tmax = kwargs.get('tmax', np.pi/(self.nuclear_gyro*b1)) 	# Default should be approximately a pi/2 pulse
 		n_steps = kwargs.get('n_steps', 1000)
-		tlist = np.linspace(0,tmax,n_steps)
+		tlist = np.linspace(0,tmax,n_steps) 	# Time vector for simulation
 		# Operator list to be evaluated at each time step
 		e_ops_list = [self.Sx_operator, self.Sy_operator, self.Sz_operator, self.Ix_operator, 
 						self.Iy_operator, self.Iz_operator]
 		drive_args = {'b1':b1, 'freq':freq, 'phase':0}
-		esr_Hamiltonian = 0.5*self.electron_gyro*self.Sx_operator
-		nmr_Hamiltonian = 0.5*self.nuclear_gyro*self.Ix_operator
+		esr_Hamiltonian = 0.5*self.electron_gyro*self.Sx_operator 	# ESR Hamiltonian
+		nmr_Hamiltonian = 0.5*self.nuclear_gyro*self.Ix_operator 	# NMR Hamiltonian
 		full_H = [self.Hamiltonian, [esr_Hamiltonian+nmr_Hamiltonian, rabi_drive]]
 		opts = qu.Options(nsteps=5000)
 		output = qu.mesolve(full_H, psi0, tlist, c_ops=[], e_ops=e_ops_list, args=drive_args, options=opts)
@@ -198,6 +225,13 @@ class SingleDonor():
 	def calculate_resonance_freqs(self):
 		'''
 		Calculate what the ESR and NMR resonance frequencies should be from current Hamiltonian eigenstates
+		Inputs: 
+			None
+		Outputs:
+			esr1_freq - ESR transition frequency for dnup to upup (ordering electron, nucleus)
+			esr2_freq - ESR transition frequency for dndn to updn
+			nmr1_freq - NMR transition frequency for dnup to dndn
+			nmr2_freq - NMR transition frequency for updn to upup
 		'''
 		# Get eigenvalues and eigenvectors of static part of current Hamiltonian
 		vals, vecs = self.Hamiltonian.eigenstates()
@@ -222,10 +256,14 @@ def detuning_pulse(t, args):
 	return (args['stop']-args['start'])*t/args['tmax']
 	
 		
-class DonorSingletTriplet(SingleDonor):
+class SingletTriplet():
 	'''
-	Class defining a singlet triplet 31P donor spin system. Needs work to include the nuclei.
+	Class defining two coupled spins as in a singlet triplet spin system.
 	'''
+	
+	# Define some physical constants
+	_mu_b = const.physical_constants['Bohr magneton'][0]
+	_mu_n = const.physical_constants['nuclear magneton'][0]
 	
 	# Define basis vectors
 	S11 = qu.basis(5,0)
@@ -233,7 +271,7 @@ class DonorSingletTriplet(SingleDonor):
 	T0 = qu.basis(5,2)
 	Tp = qu.basis(5,3)
 	S20 = qu.basis(5,4)
-	# Also updn and dnup basis - not sure if I have these the correct way round
+	# Also updn and dnup basis - I believe I have these the correct way round....
 	updn = (T0 - S11).unit()
 	dnup = (T0 + S11).unit()
 	
@@ -245,6 +283,7 @@ class DonorSingletTriplet(SingleDonor):
 		ng_factor = kwargs.get('nuclear_g_factor', 2.2632)
 		self.hyperfine = kwargs.get('default_hyperfine', 2*np.pi*114e6)
 		detuning = kwargs.get('detuning', 0)
+		self.dBx = kwargs.get('delta_Bx', 0)
 		
 		self.electron_gyro = g_factor*self._mu_b/const.hbar
 		self.nuclear_gyro = ng_factor*self._mu_n/const.hbar
@@ -252,44 +291,38 @@ class DonorSingletTriplet(SingleDonor):
 		self.b_z = b_z
 		self.dBz = delta_Bz
 		self.dBz_freq = self.electron_gyro*self.dBz/(2*np.pi)
+		self.dBx_freq = self.electron_gyro*self.dBx/(2*np.pi)
 		self.Zeeman = (self.electron_gyro*self.b_z)/(2*np.pi)
 		self.tc = tc
 		self.alpha = alpha
 		
-		self.Hamiltonian = self.build_hamiltonian(detuning)
+		self.build_hamiltonian(detuning)
 
 	def build_hamiltonian(self, detuning=0):
 		'''
 		Build the singlet-triplet Hamiltonian
 		Inputs:
 			detuning (V) - detuning from the charge degeneracy point in volts
-			alpha 		- lever arm of gate that voltage is applied to (unitless)
 		Todo:
 			- Check that detuning scaling is correct (factor of 2pi or not)
 		'''
 		# Convert Zeeman terms to frequency units
 		detuning = self.alpha*detuning*const.eV/(const.h)	# Convert from voltage to frequency
-		# Zeeman term for electrons on left and right dots
-		#~ H_zeeman_left = 0.5*self.electron_gyro*b_z*qu.tensor(qu.sigmaz(), qu.identity(2), qu.identity(2))
-		#~ H_zeeman_right = 0.5*self.electron_gyro*(b_z+dBz)*qu.tensor(qu.identity(2), qu.sigmaz(), qu.identity(2))
-		#~ J = self.calculate_exchange(detuning, tc)
-		#~ H_exchange = J*(qu.tensor(qu.sigmax(), qu.sigmax(), qu.identity(2))
-						#~ + qu.tensor(qu.sigmay(), qu.sigmay(), qu.identity(2))
-						#~ + qu.tensor(qu.sigmaz(), qu.sigmaz(), qu.identity(2)))
-		#~ H_charge = (detuning/2*(qu.tensor(qu.identity(2), qu.identity(2), qu.sigmaz())) + 
-						#~ tc*qu.tensor(qu.identity(2), qu.identity(2), qu.sigmax()))
-		#~ H = H_zeeman_left + H_zeeman_right + H_charge + H_exchange
 		
-		# Just taking 5 by 5 Hamiltonian 
+		# Just directly writing out the 5 by 5 Hamiltonian (in units of angular frequency)
 		# Defined in the basis [S11, T-, T0, T+, S20]
-		H = qu.Qobj(np.array([[detuning/2, 0, self.dBz_freq, 0, self.tc], [0, detuning/2-self.Zeeman, 0,0,0], 
-								[self.dBz_freq, 0, detuning/2,0,0], [0,0,0,detuning/2+self.Zeeman,0], 
-								[self.tc,0,0,0,-detuning/2]]))
+		H = qu.Qobj(2*np.pi*np.array([[detuning/2, self.dBx_freq, self.dBz_freq, 0, self.tc], 
+										[self.dBx_freq, detuning/2-self.Zeeman, 0,0,self.dBx_freq], 
+										[self.dBz_freq, 0, detuning/2,0,0], 
+										[0,0,0,detuning/2+self.Zeeman,0], 
+										[self.tc,self.dBx_freq,0,0,-detuning/2]]))
 		self.Hamiltonian = H
-		# return H
+		
 		
 	def build_full_hamiltonian(self, b_z=1, dBz=0.01, detuning=0, tc1=1e9, tc2=0.5e9, pauli_energy=50e9, alpha=0.1):
 		'''
+		NOTE - not ready for usage
+		Attempting to build the full Hamiltonian, including the triplet (2,0) states.
 		Make the full 8 by 8 spin Hamiltonian for singlet and triplet states on both dots
 		TODO - fix for new philosphy of fixed parameters
 		'''
@@ -310,7 +343,7 @@ class DonorSingletTriplet(SingleDonor):
 		
 	def project_eigenvecs(self, psi):
 		'''
-		Project state psi onto the basis [S11, T-, T0, T+, S20, updn, dnup]
+		Project state psi onto the basis [S11, T-, T0, T+, S20] and [updn, dnup]
 		'''
 		S11_proj = np.abs(psi.overlap(self.S11))**2
 		Tm_proj = np.abs(psi.overlap(self.Tm))**2
@@ -335,7 +368,7 @@ class DonorSingletTriplet(SingleDonor):
 		'''
 		'''
 		self.current_detuning_sweep = np.linspace(start, stop, num_points)
-		# self.eigenvals = np.zeros([num_points, 8])
+		# self.eigenvals = np.zeros([num_points, 8]) 	# For when I was playing with full 8 by 8 Hamiltonian
 		self.eigenvals = np.zeros([num_points, 5])
 		# projvecs = np.zeros([len(b_sweep), 4])
 		for ind,val in enumerate(self.current_detuning_sweep):
@@ -357,11 +390,14 @@ class DonorSingletTriplet(SingleDonor):
 		# plt.xlabel('Magnetic field (T)')
 		# plt.ylabel('Projection')
 		# plt.legend(['E up, N down', 'E up, N up'])
+		# plt.ylim([-3.1e11,-3.0e11])
 		plt.show(block=False)
 		return plt
 		
 	def do_detuning_pulse(self, start, stop, psi0, ramp_time, num_points=1000, **kwargs):
 		'''
+		NOTE - this function is currently depreciated - to do a detuning pulse it is better to use the more
+		flexible do_pulse_sequence method.
 		Run an experiment that pulses the detuning from a start value to a stop value
 		'''
 		tlist = np.linspace(0, ramp_time, num_points)
@@ -386,17 +422,26 @@ class DonorSingletTriplet(SingleDonor):
 	def do_pulse_sequence(self, lePulse, psi0, **kwargs):
 		'''
 		Run a detuning pulse sequence
+		NOTE - some weird compiler errors have been observed using the Qutip Cubic_Spline functionality - don't blame me,
+		this runs fine on my computer!
+		Inputs:
+			lePulse - pulse object as defined by PulseSequence class
+			psi0 - initial state before pulse happens
+			nsteps (optional) - max number of steps the solver can take. Sometimes needs to be increased for long sims
 		'''
-		opts = qu.Options(nsteps=100000)
+		nsteps = kwargs.get('nsteps', 500000)
+		opts = qu.Options(nsteps)
 		
 		self.build_hamiltonian(detuning=0)
 		initial_hamiltonian = self.Hamiltonian
 		conversion_factor = self.alpha*const.eV/const.h		# Convert voltage pulse to frequency units
-		detuning_hamiltonian = conversion_factor*qu.Qobj(np.array([[1/2, 0, 0, 0, 0], 
+		detuning_hamiltonian = 2*np.pi*conversion_factor*qu.Qobj(np.array([[1/2, 0, 0, 0, 0], 
 																[0, 1/2, 0,0,0], 
 																[0, 0, 1/2,0,0], 
 																[0,0,0,1/2,0], 
 																[0,0,0,0,-1/2]]))
+		# Cubic_Spline interpolation is how Qutip handles arbitrary time dependence as input. Look it up in their docs
+		# if you want to know more.
 		qutip_time_dependence = qu.Cubic_Spline(lePulse.time_vec[0], lePulse.time_vec[-1], lePulse.simulation_waveform)
 		full_H = [initial_hamiltonian, [detuning_hamiltonian, qutip_time_dependence]]
 		output = qu.mesolve(full_H, psi0, tlist=lePulse.time_vec, c_ops=[], e_ops=[], options=opts, progress_bar=True)
@@ -417,21 +462,27 @@ class DonorSingletTriplet(SingleDonor):
 		'''
 		'''
 		plt.figure(1)
-		plt.plot(lePulse.time_vec, proj_ST, lePulse.time_vec, lePulse.simulation_waveform)
+		plt.plot(lePulse.time_vec, proj_ST)#, lePulse.time_vec, lePulse.simulation_waveform)
 		plt.legend(['S11 proj', 'T- proj', 'T0 proj', 'T+ proj', 'S20 proj'])
+		plt.ylim([-0.05,1.1])
 		plt.show(block=False)
 		
 		plt.figure(2)
-		plt.plot(lePulse.time_vec, proj_UD, lePulse.time_vec, lePulse.simulation_waveform)
+		plt.plot(lePulse.time_vec, proj_UD)#, lePulse.time_vec, lePulse.simulation_waveform)
 		plt.legend(['UpDn proj', 'DnUp proj'])
+		plt.ylim([-0.05,1.1])
 		plt.show(block=False)
 		
+		plt.figure(3)
+		plt.plot(lePulse.time_vec, lePulse.simulation_waveform)
+		plt.xlabel('time')
+		plt.ylabel('Detuning voltage')
 		
 		
 class CoupledDonors(): 
 
 	'''
-	Class for a coupled donor system
+	Class for a coupled donor system. This isn't ready. Yet......
 	'''
 	
 	S11 = qu.basis(5,0)
@@ -461,10 +512,23 @@ class CoupledDonors():
 class PulseSequence():
 	'''
 	Class to encapsulate a pulsing object which can be used in a time-dependent simulation
+	Currently only supports constant voltage levels or linear ramps
 	'''
 	
 	def __init__(self, pulse_vertices, pulse_timings, dt):
 		'''
+		Inputs:
+			pulse_vertices - voltage vertices of the pulse
+			pulse_timings - timings of each pulse
+			dt - waveform time increment
+		Example:
+			pulse_vertices = [0.2, 0.2, 0, 0, -0.1]
+			pulse_timings = [1.0e-9, 10.0e-9, 6.5e-9, 20.0e-9]
+			dt = 0.1e-9
+			This would define a waveform that has a constant level at 0.2V for 1ns, then ramps from 0.2 to 0V in 
+			10ns, then keeps a constant level at 0V for 6.5ns, then ramps to -0.1V in 20ns.
+			The vertices define voltage values, and timings define the amount of time it takes to go between these
+			vertices. Length of pulse_timings should be len(pulse_vertices)-1
 		'''
 		self.pulse_vertices = pulse_vertices
 		self.pulse_times = np.cumsum([0] + pulse_timings)
@@ -473,6 +537,7 @@ class PulseSequence():
 		
 	def make_waveform(self):
 		'''
+		Make the waveform
 		'''
 		self.le_interpolator = interp.interp1d(self.pulse_times, self.pulse_vertices)
 		self.time_vec = np.arange(self.pulse_times[0], self.pulse_times[-1], self.dt)
@@ -480,12 +545,14 @@ class PulseSequence():
 		
 	def resample_waveform(self, new_dt):
 		'''
+		If you want to resample the same waveform at a different rate, you can do so.
 		'''
 		self.dt = new_dt
 		self.make_waveform()
 		
 	def plot_waveform(self):
 		'''
+		Plot the current waveform
 		'''
 		plt.plot(self.time_vec, self.simulation_waveform, 'o')
 		plt.show(block=False)
@@ -494,7 +561,8 @@ class PulseSequence():
 class PulseExperiment():
 	'''
 	Pulse experiment class to hold simulation data etc for a pulse sequence
-	After writing this I'm starting to think it is overly complicated...
+	After writing this I'm starting to think it is unnecessarily complicated... Probably I will just use do_pulse_sequence 
+	method of SingletTriplet class.
 	'''
 	
 	def __init__(self, device, lePulse):
